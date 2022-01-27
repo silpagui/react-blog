ad207b12a1433106851024129e10fa18645932a1
silpagui <silpagui.sa@gmail.com>
2022-01-27 23:06:03 +0100

    feat(SocialMedia): create SocialMedia component

---
+++ src/components/SocialMedia/SocialMedia.component.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./SocialMedia.styles.scss";
+import facebook from "../../assets/images/facebook.svg";
+import twitter from "../../assets/images/twitter.svg";
+import instagram from "../../assets/images/instagram.svg";
+import youTube from "../../assets/images/youTube.svg";
+
+export function SocialMedia({ search }) {
+	return (
+		<ul className="social-media">
+			<li>
+				<a href="#facebook">
+					<img src={facebook} alt="Facebook" />
+				</a>
+			</li>
+			<li>
+				<a href="#twitter">
+					<img src={twitter} alt="Twitter" />
+				</a>
+			</li>
+			<li>
+				<a href="#instagram">
+					<img src={instagram} alt="Instagram" />
+				</a>
+			</li>
+			<li>
+				<a href="#youtube">
+					<img src={youTube} alt="YouTube" />
+				</a>
+			</li>
+			{search}
+		</ul>
+	);
+}
