b52c9bbe286e5c84d1b1ccd109a02461b7ca802a
silpagui <silpagui.sa@gmail.com>
2022-01-27 23:13:58 +0100

    feat(Footer): create Footer component

---
+++ src/components/Footer/Footer.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.styles.scss";
+import { SocialMedia } from "../SocialMedia/SocialMedia.component";
+
+export function Footer() {
+	return (
+		<footer className="footer content-layout">
+			<p>Lorem ipsum dolor sit amet</p>
+			<SocialMedia />
+		</footer>
+	);
+}
