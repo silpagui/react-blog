a57f31087ddd83e386a52c85e7e184e226371e2f
silpagui <silpagui.sa@gmail.com>
2022-01-27 23:09:33 +0100

    feat(TechStack): create techStack section component

---
+++ src/components/TechStack/TechStack.component.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TechStack.styles.scss";
+
+export function TechStack() {
+	return (
+		<div className="tech-stack content-layout">
+			<h2 className="title">Title Here</h2>
+			<div className="tech-stack__container">
+				<img src="http://placekitten.com/100/200" alt="kitten" />
+				<img src="http://placekitten.com/100/200" alt="kitten" />
+				<img src="http://placekitten.com/100/200" alt="kitten" />
+				<img src="http://placekitten.com/100/200" alt="kitten" />
+				<img src="http://placekitten.com/100/200" alt="kitten" />
+				<img src="http://placekitten.com/100/200" alt="kitten" />
+			</div>
+		</div>
+	);
+}
