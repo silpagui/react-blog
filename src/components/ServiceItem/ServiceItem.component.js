37f62610ac9f30847e0d6a1b38f095c56a97d92f
silpagui <silpagui.sa@gmail.com>
2022-01-27 23:08:34 +0100

    feat(ServiceItem): create ServiceItem component

---
+++ src/components/ServiceItem/ServiceItem.component.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ServiceItem.styles.scss";
+import { Button } from "../Button/Button.component";
+
+export function ServiceItem() {
+	return (
+		<div className="service-item">
+			<h3 className="service-item__title">Title</h3>
+			<p className="service-item__description">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam
+			</p>
+			<Button />
+		</div>
+	);
+}
